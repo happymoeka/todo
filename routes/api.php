cbb9bf2536531f7c419e6729b41278cdc7bf8dd7
happymoeka <c1032231@planet.kanazawa-it.ac.jp>
2022-08-31 17:08:12 +0900

    Add pages

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoItemController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::resource('todos', TodoItemController::class, ['only' => [
+        'index',
+        'store',
+        'show',
+        'edit',
+        'update',
+        'destroy',
+    ]]);
 });
